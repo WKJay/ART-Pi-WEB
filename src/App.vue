c3b8c3129304b8d56d3070bccc5d93ce2aaf907f
WKJay <1931048074@qq.com>
2020-08-31 17:11:17 +0800

    menu test

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,63 @@
 <template>
-  <div id="app">
-    hello world
-    <a-button type="primary">
-      Primary
-    </a-button>
-    <a-button>Default</a-button>
-    <a-button type="dashed">
-      Dashed
-    </a-button>
-    <a-button type="danger">
-      Danger
-    </a-button>
-    <a-config-provider :auto-insert-space-in-button="false">
-      <a-button type="primary">
-        按钮
-      </a-button>
-    </a-config-provider>
-    <a-button type="primary">
-      按钮
-    </a-button>
-    <a-button type="link">
-      Link
-    </a-button>
+  <div>
+    <a-menu v-model:selectedKeys="current" mode="horizontal" theme="dark">
+      <a-menu-item>
+        <mail-outlined />Navigation One </a-menu-item>
+      <a-menu-item key="app" disabled>
+        <appstore-outlined />Navigation Two </a-menu-item>
+      <a-sub-menu>
+        <template v-slot:title>
+          <span class="submenu-title-wrapper">
+            <setting-outlined />
+            Navigation Three - Submenu
+          </span>
+        </template>
+        <a-menu-item-group title="Item 1">
+          <a-menu-item key="setting:1">
+            Option 1
+          </a-menu-item>
+          <a-menu-item key="setting:2">
+            Option 2
+          </a-menu-item>
+        </a-menu-item-group>
+        <a-menu-item-group title="Item 2">
+          <a-menu-item key="setting:3">
+            Option 3
+          </a-menu-item>
+          <a-menu-item key="setting:4">
+            Option 4
+          </a-menu-item>
+        </a-menu-item-group>
+      </a-sub-menu>
+      <a-menu-item key="alipay">
+        <a href="https://antdv.com" target="_blank" rel="noopener noreferrer">
+          Navigation Four - Link
+        </a>
+      </a-menu-item>
+    </a-menu>
   </div>
 </template>
-
 <script>
-  export default {
-    name: 'App',
-    components: {
-
-    }
-  }
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons-vue';
+export default {
+  components: {
+    MailOutlined,
+    AppstoreOutlined,
+    SettingOutlined,
+  },
+  data() {
+    return {
+      current: ['mail'],
+    };
+  },
+};
 </script>
 
 <style>
-
+.ant-menu-dark {
+  background-color: #343a40 !important;
+}
+.ant-menu-sub {
+  background-color: #343a40 !important;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import {Button} from 'ant-design-vue';
+import { Button, Menu } from 'ant-design-vue';
 import 'ant-design-vue/lib/button/style';
+import 'ant-design-vue/lib/menu/style';
 
-const app= createApp(App);
+const app = createApp(App);
 app.use(Button);
+app.use(Menu);
 app.mount('#app');
